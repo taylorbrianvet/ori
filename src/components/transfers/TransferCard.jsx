bd170a9c45e4bdd7db72db5fe6b1a6182527d31d
Brian Taylor <31briantaylor@gmail.com>
2026-03-11 03:08:03 +0000

    File changes

--- src/components/transfers/TransferCard.jsx
+++ src/components/transfers/TransferCard.jsx
@@ -5,10 +5,13 @@ import TransferDetailModal from "./TransferDetailModal";
 
 function formatLocalTime(isoString) {
   if (!isoString) return "";
-  const d = new Date(isoString);
+  // Ensure UTC parsing by appending Z if no timezone info present
+  const s = /[Z+\-]\d*$/.test(isoString) ? isoString : isoString + "Z";
+  const d = new Date(s);
   return d.toLocaleString(undefined, {
     month: "short", day: "numeric",
     hour: "numeric", minute: "2-digit", hour12: true,
+    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   });
 }
 
