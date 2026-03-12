b73143c1d5edc3c1415a82a7f27ab7b4cae30d3d
Brian Taylor <31briantaylor@gmail.com>
2026-03-12 03:18:58 +0000

    File changes

--- src/pages/ServiceBoard.jsx
+++ src/pages/ServiceBoard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
